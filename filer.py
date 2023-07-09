6cbeee3ee9c02e69d61324adb831e2d2f5c4f740
Cameron Candau <camcandau@gmail.com>
2023-07-09 11:02:50 -0700

    fixed bugs, added comments, program updates config

--- filer.py
+++ filer.py
@@ -6,59 +6,67 @@ import time
 from pytrie import StringTrie
 
 class Filer:
-    flag_pattern = r'[^_]+__'
-    flag_dir_trie = StringTrie()
-    source_dir = ""
+    flag_pattern = r'[^_]+__'   # matches flags in a string
+    flag_dir_trie = StringTrie() # trie to store flag prefixes
 
     def __init__(self, conf_file):
-        with open(conf_file, 'r') as fin:
-            self.source_input = fin.readline().strip()
-            self.source_dir = Path(self.source_input).expanduser()
+        self.__config = conf_file
+        with open(self.__config, 'r') as fin:
+            source_input = fin.readline().strip()  # source path stored in first line of config
+            self.__source_dir = Path(source_input).expanduser()
 
             for line in fin.readlines():
                 flag, conf_path = line.split(' ')
                 self.flag_dir_trie[flag] = conf_path.strip()
 
-            for key, val in self.flag_dir_trie.items():
-                print(f"{key}, {val}")
+    def write_config(self, line):
+        with open(self.__config, 'a') as fin:
+            fin.write(line + '\n')
+
 
     def run(self):
         while True:
-            files = os.listdir(self.source_dir)
+            files = os.listdir(self.__source_dir)
             for filename in files:
-                remaining_flags = ""
                 flag_matches = re.findall(self.flag_pattern, filename)
                 if not flag_matches:
                     continue
 
+                first_flag = flag_matches[0]
                 last_flag = flag_matches[-1]
+                first_flag_index = filename.rfind(first_flag)
                 last_flag_index = filename.rfind(last_flag) + len(last_flag)
                 new_name = filename[last_flag_index:]
+                all_flags = filename[first_flag_index:last_flag_index]
 
                 print(f"FLAG_MATCHES: {flag_matches}")
                 dest_dir = "~/"
+                contained_flags = ""
+                remaining_flags = ""
                 for i in range(len(flag_matches) - 1, -1, -1):
-                    print(f"i: {i}")
-                    print(f"flag match: {flag_matches[i]}")
-                    contained_flags = "".join(flag_matches[:i])
-                    print(f"CONTAINED_FLAGS: {contained_flags}")
-                    '''
-                    if contained_flags not in self.flag_dir_trie:
+                    candidate_flags = "".join(flag_matches[:i])
+                    if candidate_flags not in self.flag_dir_trie:
                         continue
-                    '''
-
+                    contained_flags = candidate_flags
                     remaining_flags = "".join(flag_matches[i:])
-                    print(f"REMAINING_FLAGS: {remaining_flags}")
-                    remaining_dir = remaining_flags.replace("__", "/")
-                    print(remaining_dir)
-                    contained_dir = self.flag_dir_trie[contained_flags] if len(contained_flags) > 0 else "~/"
-                    dest_dir = contained_dir + remaining_dir + new_name
                     break
 
-                dest_path = Path(dest_dir).expanduser()
+                print(f"CONTAINED_FLAGS: {contained_flags} -> {self.flag_dir_trie[contained_flags]}")
+                remaining_dir = remaining_flags.replace("__", "/")
+                print(f"REMAINING_FLAGS: {remaining_flags} -> {remaining_dir}")
+                contained_dir = self.flag_dir_trie[contained_flags] if len(contained_flags) > 0 else "~/"
+                dest_dir = contained_dir + remaining_dir
+
+                # update trie and config file if there were any new flags
+                if len(remaining_flags) > 0 and all_flags not in self.flag_dir_trie:
+                    self.flag_dir_trie[all_flags] = dest_dir
+                    self.write_config(f"{all_flags} {dest_dir}")
+
+                dest_path = Path(dest_dir + new_name).expanduser()
+                print(dest_path)
 
                 os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
-                shutil.move(os.path.join(str(self.source_dir), filename), dest_path)
+                shutil.move(os.path.join(str(self.__source_dir), filename), dest_path)
 
             time.sleep(0.01)
