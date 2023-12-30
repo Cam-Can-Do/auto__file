b851110bd5733782f03abbf968271a8bf756c1f7
Cameron Candau <camcandau@gmail.com>
2023-12-29 16:20:16 -0800

    removed looping and made minor improvements

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# auto__file
+# auto__filer
 A file sorter application for Linux that follows rules stored in a configuration file, but also sorts dynamically in the absense of an appropriate rule.
 
 A program that monitors a "source" directory (expected to be specified in the first non-commented line of config file) for files that contain "__" (double underscores) in their name. Text followed by a double underscore is a flag. Flags can be specified in .config.txt, and act as a shortcut to a path on the system. See comments in '.config.txt' for more information.
@@ -7,4 +7,4 @@ The source directory and flags are read into the program and stored in a trie. I
 
 When the program detects a filename in the source directory that contains a flag, it constructs a destination path and renames the file to strip the original flags and move it to the appropriate directory. If a filename contains flags that are not present in the config file, the program assumes that the first flag should be a directory within the user's home directory. New flags and their assumed paths are automatically added to the trie and config file.
 
-Each action performed by the program is logged.
+Each action performed by the program is logged in `auto__file.log`.
--- filer.py
+++ filer.py
@@ -21,19 +21,22 @@ class Filer:
             # read flags and their corresponding path from each line of config
             for line in fin.readlines():
                 line = line.strip()
-                if line.startswith("#") or line == "":    # ignore comments
+                if line.startswith("#") or line == "":    # ignore comments and empty lines
                     continue
-                # source dir is expected as the first valid line
+                # source dir is expected as the first valid line of the config
                 if not self.__source_dir:   
                     self.__source_dir = Path(line).expanduser()
                 else:
                     flag, conf_path = line.split("/->/")
                     self.__flag_dir_trie[flag] = conf_path
 
+        # Log program start, config file import, and rules imported.
+        logging.info("=" * 50)
+        logging.info("AUTO__FILER RUNNING")
+        logging.info("=" * 50)
         logging.info("Config file imported:")
         for flag, path in self.__flag_dir_trie.items():
             logging.info(f"\t{flag} -> {path}")
-        logging.info("=" * 50)
         logging.info("Files:")
 
     # writes a string to the config file
@@ -45,81 +48,77 @@ class Filer:
 
 
     def run(self):
-        while True:
-            # 'ls' in __source_dir to parse filenames
-            files = os.listdir(self.__source_dir)
-            for filename in files:
-                flag_matches = re.findall(self.__flag_pattern, filename)
-                if not flag_matches:    # if no flags are found in a filename skip it
+        # 'ls' in __source_dir to parse filenames
+        files = os.listdir(self.__source_dir)
+        for filename in files:
+            flag_matches = re.findall(self.__flag_pattern, filename)
+            if not flag_matches:    # if no flags are found in a filename skip it
+                continue
+            logging.info("-" * 50)
+            logging.info(f"File {filename} has flags: {flag_matches}")
+
+            if " " in filename:
+                filename.replace(" ", "_")
+
+            # for seperating flags vs name in filename
+            first_flag = flag_matches[0]
+            last_flag = flag_matches[-1]
+            first_flag_index = filename.rfind(first_flag)
+            last_flag_index = filename.rfind(last_flag) + len(last_flag)
+
+            # everthing after the last flag
+            new_name = filename[last_flag_index:]
+            # everything between (including) the first and last flags
+            all_flags = filename[first_flag_index:last_flag_index]
+
+
+            # consecutuve flags already present in self.__flag_dir_trie
+            contained_flags = ""
+            # flags not yet recorded
+            remaining_flags = ""
+
+            # starting at the last flag, find the longest series of 
+            # consecutive flags that's in the trie
+            for i in range(len(flag_matches), -1, -1):
+                candidate_flags = "".join(flag_matches[:i])
+                if candidate_flags not in self.__flag_dir_trie:
                     continue
-                logging.info("-" * 50)
-                logging.info(f"File {filename} has flags: {flag_matches}")
+                else:   # if current candidate flags are in the trie
+                    contained_flags = candidate_flags
+                    remaining_flags = "".join(flag_matches[i:])
+                    break
 
-                if " " in filename:
-                    filename.replace(" ", "_")
+            # set contained_flags -- if none, needs to be "~/"" for the path's start
+            if len(contained_flags) > 0:
+                contained_dir = self.__flag_dir_trie[contained_flags]
+            else:
+                contained_dir = "~/"
+                remaining_flags = all_flags
 
-                # for seperating flags vs name in filename
-                first_flag = flag_matches[0]
-                last_flag = flag_matches[-1]
-                first_flag_index = filename.rfind(first_flag)
-                last_flag_index = filename.rfind(last_flag) + len(last_flag)
-
-                # everthing after the last flag
-                new_name = filename[last_flag_index:]
-                # everything between (including) the first and last flags
-                all_flags = filename[first_flag_index:last_flag_index]
+            # convert remaining flags to path form, each representing a dir
+            remaining_dir = remaining_flags.replace("__", "/")
 
 
-                # consecutuve flags already present in self.__flag_dir_trie
-                contained_flags = ""
-                # flags not yet recorded
-                remaining_flags = ""
+            logging.info(f"Contained flag mappings: {contained_flags} -> {contained_dir}")
+            logging.info(f"Remaining flags: {remaining_flags} -> {remaining_dir}")
 
-                # starting at the last flag, find the longest series of 
-                # consecutive flags that's in the trie
-                for i in range(len(flag_matches), -1, -1):
-                    candidate_flags = "".join(flag_matches[:i])
-                    if candidate_flags not in self.__flag_dir_trie:
-                        continue
-                    else:   # if current candidate flags are in the trie
-                        contained_flags = candidate_flags
-                        remaining_flags = "".join(flag_matches[i:])
-                        break
+            dest_dir = contained_dir + remaining_dir
 
-                # set contained_flags -- if none, needs to be "~/"" for the path's start
-                if len(contained_flags) > 0:
-                    contained_dir = self.__flag_dir_trie[contained_flags]
-                else:
-                    contained_dir = "~/"
-                    remaining_flags = all_flags
+            # update trie and config file if there were any new flags
+            if all_flags not in self.__flag_dir_trie:
+                self.add_entry(all_flags, dest_dir)
 
-                # convert remaining flags to path form, each representing a dir
-                remaining_dir = remaining_flags.replace("__", "/")
+            dest_path = Path(dest_dir).expanduser()
+            dest_name = dest_path / new_name
+            logging.info(f"Dest path: {dest_path}")
 
-
-                logging.info(f"Contained flag mappings: {contained_flags} -> {contained_dir}")
-                logging.info(f"Remaining flags: {remaining_flags} -> {remaining_dir}")
-
-                dest_dir = contained_dir + remaining_dir
-
-                # update trie and config file if there were any new flags
-                if all_flags not in self.__flag_dir_trie:
-                    self.add_entry(all_flags, dest_dir)
-
-                dest_path = Path(dest_dir).expanduser()
-                dest_name = dest_path / new_name
-                logging.info(f"Dest path: {dest_path}")
-
-                # create the destination directory if it doesn't exist
-                os.makedirs(dest_path, exist_ok=True)
-                logging.info(f"Created dir: {Path(dest_dir).expanduser()}")
-                # rename/move file to the destination indicated by its flags,
-                # new name no longer contains flags
-                logging.info(f"Moved {str(Path(self.__source_dir).joinpath(filename))} to {dest_name}")
-                shutil.move(Path(self.__source_dir).joinpath(filename), dest_name)
-
-            # time delay to mitigate overhead of continuously running the loop
-            time.sleep(0.1)
+            # create the destination directory if it doesn't exist
+            os.makedirs(dest_path, exist_ok=True)
+            logging.info(f"Created dir: {Path(dest_dir).expanduser()}")
+            # rename/move file to the destination indicated by its flags,
+            # new name no longer contains flags
+            logging.info(f"Moved {str(Path(self.__source_dir).joinpath(filename))} to {dest_name}")
+            shutil.move(Path(self.__source_dir).joinpath(filename), dest_name)
 
 # Run program
 if __name__ == "__main__":
